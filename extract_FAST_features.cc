2f939d30bb9a04ba5517e809023074e1ec2c7b2d
Edward Rosten <ed@edwardrosten.com>
2019-09-14 21:15:44 +0100

    Make extract work on more varied input, avoid classification and fix bugs

--- extract_FAST_features.cc
+++ extract_FAST_features.cc
@@ -45,13 +45,21 @@ features should be extracted are specified on the commandline.
 #include <map>
 #include <iterator>
 #include <string>
+#include <string_view>
+#include <array>
+#include <algorithm>
 
 
 using namespace CVD;
-using namespace std;
+using std::string;
+using std::vector;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::vector;
 using namespace GVars3;
 
-ImageRef dir[17]=
+ImageRef dir[16]=
 {
 	ImageRef(0,3),
 	ImageRef(1,3),
@@ -71,50 +79,26 @@ ImageRef dir[17]=
 	ImageRef(-1,3),
 };
 
-///Determine if a string has the properties of a FAST-N corner.
-///In other words, if it has enough consecutive characters of the correct
-///type. This function assumes that the string wraps in a circular manner.
-///@param str String to test for cornerness.
-///@param num_for_corner Number of consecutive characters required for corner
-///@param type Character value which must appear consecutively#
-///@return whether the string is a corner.
-inline bool is_corner(const char* str, int num_for_corner, char type)
-{
-        int num_consecutive=0;
-        int first_cons=0;
+using Feature = std::array<char,16>;
+bool operator<(const Feature& f1, const Feature& f2){
+	return std::lexicographical_compare(f1.begin(), f1.end(), f2.begin(), f2.end());
+}
 
-        for(int i=0; i<16; i++)
-        {
-                if(str[i] == type)
-                {
-                        num_consecutive++;
-
-                        if(num_consecutive == num_for_corner)
-                                return 1;
-                } 
-                else
-                {
-                        if(num_consecutive == i)
-                                first_cons=i;
-
-                        num_consecutive=0;
-                }
-        }
-
-        if(first_cons+num_consecutive >=num_for_corner)
-                return 1;
-        else 
-                return 0;
+namespace std {
+	template <> struct hash<Feature> {
+		std::size_t operator()(const Feature& t) const {
+			return std::hash<std::string_view>()({t.data(), t.size()});
+		}
+	};
 }
 
 
-string extract(const BasicImage<byte>& imp, ImageRef pos, int threshold)
+Feature extract(const BasicImage<byte>& imp, ImageRef pos, int threshold)
 {
 	int cb = imp[pos] + threshold;
-	int c_b = imp[pos] + threshold;
+	int c_b = imp[pos] - threshold;
 	
-	string res;
-	res.resize(16);
+	Feature res;
 	for(int i=0; i < 16; i++)
 		if(imp[pos + dir[i]] > cb)
 			res[i] = 'b';
@@ -125,6 +109,14 @@ string extract(const BasicImage<byte>& imp, ImageRef pos, int threshold)
 	return res;
 }
 
+void extract_features(const SubImage<byte>& im, int t, std::unordered_map<Feature, uint64_t>& features){
+	for(int y=3; y < im.size().y-3; y++)
+		for(int x=3; x < im.size().x-3; x++)
+			features[extract(im, ImageRef(x, y), t)]++;
+}
+
+
+
 ///Driving program
 ///@param argc Number of commandline arguments
 ///@param argv List of commandline arguments. Contains GVars3 arguments, and images to process.
@@ -133,32 +125,62 @@ int main(int argc, char** argv)
 	int lastarg = GUI.parseArguments(argc, argv);
 	
 	int t = GV3::get<int>("threshold", 10);
-	int N = GV3::get<int>("N", 9);
+	bool augment = GV3::get<int>("augment", true);
 	int frames = GV3::get<int>("frames", 1000);
-
-	VideoBuffer<byte>* vbuf = open_video_source<byte>(GV3::get<string>("video", "", FATAL_IF_NOT_DEFINED));
-
-	map<string, uint64_t> corners;
-	map<string, uint64_t> non_corners;
-
-	for(int nf = 0; nf < frames; nf++)
-	{
-		VideoFrame<byte>* vf = vbuf->get_frame();
-
-		for(int y=3; y < vf->size().y-3; y++)
-			for(int x=3; x < vf->size().x-3; x++)
-			{
-				string s = extract(*vf, ImageRef(x, y), t);
-
-				if(is_corner(s.c_str(), N, 'b') || is_corner(s.c_str(), N, 'd'))
-					corners[s]++;
-				else
-					non_corners[s]++;
-			}
-
-		vbuf->put_frame(vf);
 	
-		clog << nf << endl;
+	std::unordered_map<Feature, uint64_t> features;
+	
+	for(int arg=lastarg; arg < argc; arg++){
+		
+		cerr << "Processing " << argv[arg] << " ";
+
+		try{
+			Image<byte> im = img_load(argv[arg]);			
+			extract_features(im, t, features);
+			cerr << "image ";
+		}
+		catch(CVD::Exceptions::All&){
+			try{
+				VideoBuffer<byte>* vbuf = open_video_source<byte>(argv[arg]);
+
+				cerr << "video ";
+				for(int nf = 0; nf < frames; nf++)
+				{
+					VideoFrame<byte>* vf = vbuf->get_frame();
+					extract_features(*vf, t, features);
+					vbuf->put_frame(vf);
+					cerr << nf << " ";
+				}
+			}
+			catch(CVD::Exceptions::All&){
+				cerr << "bad ";	
+			}
+		}
+		cerr << features.size() << endl;
+	}
+	
+
+	if(augment){
+		char types[]="bsd.";
+		Feature F;
+		int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
+		for(a = 0, F[ 0]='b'; a < 3; a++, F[ 0]=types[a])
+		 for(b = 0, F[ 1]='b'; b < 3; b++, F[ 1]=types[b])
+		  for(c = 0, F[ 2]='b'; c < 3; c++, F[ 2]=types[c])
+		   for(d = 0, F[ 3]='b'; d < 3; d++, F[ 3]=types[d])
+			for(e = 0, F[ 4]='b'; e < 3; e++, F[ 4]=types[e])
+			 for(f = 0, F[ 5]='b'; f < 3; f++, F[ 5]=types[f])
+			  for(g = 0, F[ 6]='b'; g < 3; g++, F[ 6]=types[g])
+			   for(h = 0, F[ 7]='b'; h < 3; h++, F[ 7]=types[h])
+				for(i = 0, F[ 8]='b'; i < 3; i++, F[ 8]=types[i])
+				 for(j = 0, F[ 9]='b'; j < 3; j++, F[ 9]=types[j])
+				  for(k = 0, F[10]='b'; k < 3; k++, F[10]=types[k])
+				   for(l = 0, F[11]='b'; l < 3; l++, F[11]=types[l])
+					for(m = 0, F[12]='b'; m < 3; m++, F[12]=types[m])
+					 for(n = 0, F[13]='b'; n < 3; n++, F[13]=types[n])
+					  for(o = 0, F[14]='b'; o < 3; o++, F[14]=types[o])
+					   for(p = 0, F[15]='b'; p < 3; p++, F[15]=types[p])
+							features[F]++;
 	}
 
 	cout << 16 << endl;
@@ -166,9 +188,7 @@ int main(int argc, char** argv)
 		cout << dir[i] << " ";
 	cout << endl;
 
-	for(map<string, uint64_t>::iterator i=corners.begin(); i != corners.end(); i++)
-		cout << i->first << " " << i->second << " 1" << endl;
-	for(map<string, uint64_t>::iterator i=non_corners.begin(); i != non_corners.end(); i++)
-		cout << i->first << " " << i->second << " 0" << endl;
+	for(const auto& f:features)
+		cout << string(f.first.data(), f.first.size())  << " " << f.second << endl;
 
 }
